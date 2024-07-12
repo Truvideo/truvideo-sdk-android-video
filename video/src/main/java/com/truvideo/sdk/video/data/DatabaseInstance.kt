7b67963523fa9c9221b0371cf203e682d432f324
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-07-12 14:02:46 +0000

    Update 73.1.1-beta.62

---
+++ video/src/main/java/com/truvideo/sdk/video/data/DatabaseInstance.kt
@@ -0,0 +1,20 @@
+package com.truvideo.sdk.video.data
+
+import android.content.Context
+import androidx.room.Room
+
+internal object DatabaseInstance {
+    private var instance: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        return instance ?: synchronized(this) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+    }
+
+    private fun buildDatabase(context: Context): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, "app-video")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
