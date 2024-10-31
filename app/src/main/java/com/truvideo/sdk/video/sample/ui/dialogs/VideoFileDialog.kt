6d5919a38c6d31aed06402d294f3524592a5062a
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-10-31 21:04:56 +0000

    Update 76.1.3-rc.87

---
+++ app/src/main/java/com/truvideo/sdk/video/sample/ui/dialogs/VideoFileDialog.kt
@@ -0,0 +1,135 @@
+package com.truvideo.sdk.video.sample.ui.dialogs
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.MissedVideoCall
+import androidx.compose.material.icons.automirrored.outlined.VolumeMute
+import androidx.compose.material.icons.outlined.Audiotrack
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.FileCopy
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.NoiseAware
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.Videocam
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.truvideo.sdk.video.sample.ui.components.option_dialog_content.OptionDialogContent
+import com.truvideo.sdk.video.sample.ui.components.option_dialog_content.OptionDialogModel
+import com.truvideo.sdk.video.sample.ui.theme.TruVideoSdkVideoModuleTheme
+import kotlinx.collections.immutable.persistentListOf
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VideoFileDialog(
+    visible: Boolean = true,
+    close: () -> Unit = {},
+    onInfoPressed: () -> Unit = {},
+    onPlayPressed: () -> Unit = {},
+    onEditPressed: () -> Unit = {},
+    onClearNoisePressed: () -> Unit = {},
+    onDuplicatePressed: () -> Unit = {},
+    onRemoveAudioPressed: () -> Unit = {},
+    onRemoveVideoPressed: () -> Unit = {},
+    onAddVideoTrackPressed: () -> Unit = {},
+    onAddAudioTrackPressed: () -> Unit = {},
+    onDeletePressed: () -> Unit = {}
+) {
+    if (visible) {
+        BasicAlertDialog(
+            onDismissRequest = { close() }
+        ) {
+            Content(
+                onInfoPressed = onInfoPressed,
+                onRemoveAudioPressed = onRemoveAudioPressed,
+                onClearNoisePressed=onClearNoisePressed,
+                onRemoveVideoPressed = onRemoveVideoPressed,
+                onAddVideoTrackPressed = onAddVideoTrackPressed,
+                onAddAudioTrackPressed = onAddAudioTrackPressed,
+                onDuplicatePressed = onDuplicatePressed,
+                onPlayPressed = onPlayPressed,
+                onEditPressed = onEditPressed,
+                onDeletePressed = onDeletePressed
+            )
+        }
+    }
+}
+
+@Composable
+private fun Content(
+    onInfoPressed: () -> Unit = {},
+    onPlayPressed: () -> Unit = {},
+    onEditPressed: () -> Unit = {},
+    onClearNoisePressed: () -> Unit = {},
+    onDuplicatePressed: () -> Unit = {},
+    onRemoveAudioPressed: () -> Unit = {},
+    onRemoveVideoPressed: () -> Unit = {},
+    onAddVideoTrackPressed: () -> Unit = {},
+    onAddAudioTrackPressed: () -> Unit = {},
+    onDeletePressed: () -> Unit = {}
+) {
+    OptionDialogContent(
+        title = "Video",
+        options = persistentListOf(
+            OptionDialogModel(
+                title = "Information",
+                icon = Icons.Outlined.Info,
+                onPressed = onInfoPressed
+            ),
+            OptionDialogModel(
+                title = "Play",
+                icon = Icons.Outlined.PlayArrow,
+                onPressed = onPlayPressed
+            ),
+            OptionDialogModel(
+                title = "Edit",
+                icon = Icons.Outlined.Edit,
+                onPressed = onEditPressed
+            ),
+            OptionDialogModel(
+                title = "Clear noise",
+                icon = Icons.Outlined.NoiseAware,
+                onPressed = onClearNoisePressed
+            ),
+            OptionDialogModel(
+                title = "Duplicate",
+                icon = Icons.Outlined.FileCopy,
+                onPressed = onDuplicatePressed
+            ),
+            OptionDialogModel(
+                title = "Remove audio",
+                icon = Icons.AutoMirrored.Outlined.VolumeMute,
+                onPressed = onRemoveAudioPressed
+            ),
+            OptionDialogModel(
+                title = "Remove video",
+                icon = Icons.AutoMirrored.Outlined.MissedVideoCall,
+                onPressed = onRemoveVideoPressed
+            ),
+            OptionDialogModel(
+                title = "Add video track",
+                icon = Icons.Outlined.Videocam,
+                onPressed = onAddVideoTrackPressed
+            ),
+            OptionDialogModel(
+                title = "Add audio track",
+                icon = Icons.Outlined.Audiotrack,
+                onPressed = onAddAudioTrackPressed
+            ),
+            OptionDialogModel(
+                title = "Delete",
+                icon = Icons.Outlined.Delete,
+                onPressed = onDeletePressed
+            )
+        )
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Test() {
+    TruVideoSdkVideoModuleTheme {
+        Content()
+    }
+}
---
+++ video/src/main/java/com/truvideo/sdk/video/usecases/ClearNoiseUseCase.kt
@@ -0,0 +1,118 @@
+package com.truvideo.sdk.video.usecases
+
+import android.content.Context
+import com.truvideo.krisplibrary.Krisp
+import com.truvideo.krisplibrary.Utils
+import com.truvideo.krisplibrary.listener.KrispListener
+import com.truvideo.sdk.video.model.TruvideoSdkVideoFile
+import com.truvideo.sdk.video.model.TruvideoSdkVideoFileDescriptor
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import truvideo.sdk.common.exceptions.TruvideoSdkException
+import java.io.File
+import kotlin.coroutines.suspendCoroutine
+
+internal class ClearNoiseUseCase(
+    private val context: Context,
+    private val extractAudioUseCase: ExtractAudioUseCase,
+    private val replaceAudioTrackUseCase: ReplaceAudioTrackUseCase
+) {
+
+    companion object {
+        private const val TEMP_AUDIO_FILE_NAME = "audio_temp"
+        private const val TEMP_AUDIO_CLEANED_FILE_NAME = "audio_temp_cleaned"
+    }
+
+    private val mutex = Mutex()
+
+    suspend operator fun invoke(
+        input: TruvideoSdkVideoFile,
+        output: TruvideoSdkVideoFileDescriptor
+    ) {
+        mutex.withLock {
+            val filesToDelete = mutableListOf<String>()
+
+            try {
+                // Extract audio from the video file
+                val audioPath = extractAudioUseCase(
+                    input = input,
+                    output = TruvideoSdkVideoFileDescriptor.cache(TEMP_AUDIO_FILE_NAME)
+                )
+                filesToDelete.add(audioPath)
+
+                // Get cleaned audio byte array
+                val cleanedAudioByteArray = clearNoise(TruvideoSdkVideoFile.cache(TEMP_AUDIO_FILE_NAME, File(audioPath).extension))
+
+                // Create cleaned audio file
+                val cleanedAudioFile = TruvideoSdkVideoFile.cache(
+                    TEMP_AUDIO_CLEANED_FILE_NAME,
+                    File(audioPath).extension
+                )
+                val cleanedAudioFilePath = cleanedAudioFile.getPath(context)
+                tryDeleteFile(cleanedAudioFilePath)
+                filesToDelete.add(cleanedAudioFilePath)
+
+                // Move byte array to file
+                Utils.byteArrayToFile(File(cleanedAudioFilePath), cleanedAudioByteArray)
+
+                // Merge cleaned audio to final video
+                replaceAudioTrackUseCase(
+                    videoInput = input,
+                    audioInput = cleanedAudioFile,
+                    output = output
+                )
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+
+                if (exception is TruvideoSdkException) {
+                    throw exception
+                } else {
+                    throw TruvideoSdkException("Unknown error")
+                }
+            } finally {
+                filesToDelete.forEach {
+                    tryDeleteFile(it)
+                }
+            }
+        }
+    }
+
+
+    private suspend fun clearNoise(input: TruvideoSdkVideoFile): ByteArray {
+        return suspendCoroutine { continuation ->
+            try {
+                val bytes = Utils.fileToByteArray(File(input.getPath(context)))
+                Krisp().call(context,
+                    bytes,
+                    object : KrispListener {
+                        override fun onSuccess(byteArray: ByteArray?) {
+                            if (byteArray == null) {
+                                continuation.resumeWith(Result.failure(TruvideoSdkException("File not found")))
+                            } else {
+                                continuation.resumeWith(Result.success(byteArray))
+                            }
+                        }
+
+                        override fun onError() {
+                            continuation.resumeWith(Result.failure(TruvideoSdkException("Unknown error")))
+                        }
+                    }
+                )
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                if (ex is TruvideoSdkException) {
+                    continuation.resumeWith(Result.failure(ex))
+                } else {
+                    continuation.resumeWith(Result.failure(TruvideoSdkException("Unknown error")))
+                }
+            }
+        }
+    }
+
+    private fun tryDeleteFile(path: String) {
+        try {
+            File(path).delete()
+        } catch (_: Exception) {
+        }
+    }
+}
