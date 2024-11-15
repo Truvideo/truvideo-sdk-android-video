d8ce23d0f29445d32a3fabbbaacd798cf6263e3e
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2024-11-15 19:11:25 +0000

    Update 76.2.5-rc.95

---
+++ video/src/main/java/com/truvideo/sdk/video/examples/CompareJava.java
@@ -0,0 +1,30 @@
+package com.truvideo.sdk.video.examples;
+
+import androidx.annotation.NonNull;
+
+import com.truvideo.sdk.video.TruvideoSdkVideo;
+import com.truvideo.sdk.video.interfaces.TruvideoSdkVideoCallback;
+import com.truvideo.sdk.video.model.TruvideoSdkVideoFile;
+
+import java.util.List;
+
+import truvideo.sdk.common.exceptions.TruvideoSdkException;
+
+public class CompareJava {
+
+    public void compare(List<TruvideoSdkVideoFile> input) {
+        TruvideoSdkVideo.getInstance().compare(input, new TruvideoSdkVideoCallback<Boolean>() {
+            @Override
+            public void onComplete(Boolean result) {
+                // Handle result
+                // result is true if the videos are compatible to be concatenated, otherwise false
+            }
+
+            @Override
+            public void onError(@NonNull TruvideoSdkException exception) {
+                // Handle error
+                exception.printStackTrace();
+            }
+        });
+    }
+}
